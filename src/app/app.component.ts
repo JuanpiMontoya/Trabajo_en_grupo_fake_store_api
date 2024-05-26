925cbd844cca203203b7a1c46080580440c4c94d
Juan Pablo Montoya <juan.montoya@ucb.edu.bo>
2024-05-26 17:44:49 -0400

    Primer Avance Trabajo - Header y Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Trabajo_en_grupo_fake_store';
+  title = 'mi-tienda-con-fake-store-api';
 }
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './paginas/inicio/inicio.component';
+import { TiendaComponent } from './paginas/tienda/tienda.component';
+import { NosotrosComponent } from './paginas/nosotros/nosotros.component';
+import { PaginaNoEncontradaComponent } from './paginas/pagina-no-encontrada/pagina-no-encontrada.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'inicio', component: InicioComponent },
+    { path: 'tienda', component: TiendaComponent },
+    { path: 'nosotros', component: NosotrosComponent },
+    { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+    { path: '**', component: PaginaNoEncontradaComponent }
+];
