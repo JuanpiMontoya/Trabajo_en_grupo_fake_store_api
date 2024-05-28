a878dce155f6f4caa65de76c8998b6262530e1ce
Wenba1 <balderramawendym@gmail.com>
2024-05-28 10:48:17 -0400

    Avance de About

--- src/app/paginas/nosotros/nosotros.component.spec.ts
+++ src/app/paginas/nosotros/nosotros.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgModule } from '@angular/core'; 
 
 import { NosotrosComponent } from './nosotros.component';
 
@@ -8,7 +9,7 @@ describe('NosotrosComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NosotrosComponent]
+      imports: [ ]
     })
     .compileComponents();
     
