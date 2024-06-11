48b7fe40009f9c4bf92489f80e5ee70d67e4916f
Wenba1 <balderramawendym@gmail.com>
2024-06-10 21:48:33 -0400

    Iniciar Sesion + Crear una cuenta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'mi-tienda-con-fake-store-api';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { TiendaComponent } from './paginas/tienda/tienda.component';
 import { NosotrosComponent } from './paginas/nosotros/nosotros.component';
 import { PaginaNoEncontradaComponent } from './paginas/pagina-no-encontrada/pagina-no-encontrada.component';
 import { DetallesComponent } from './paginas/detalles/detalles.component';
+import { IniciarSesionComponent } from './paginas/iniciar-sesion/iniciar-sesion.component';
+import { CrearCuentaComponent } from './paginas/crear-cuenta/crear-cuenta.component';
 import { NgModule } from '@angular/core';
 
 
@@ -11,9 +13,12 @@ export const routes: Routes = [
     { path: 'inicio', component: InicioComponent },
     { path: 'tienda', component: TiendaComponent },
     { path: 'nosotros', component: NosotrosComponent },
+    { path: 'iniciar-sesion', component: IniciarSesionComponent },
+    { path: 'crear-cuenta', component: CrearCuentaComponent },
     { path: '', redirectTo: 'inicio', pathMatch: 'full' },
     { path: 'detalles/:id', component: DetallesComponent },
-    { path: '**', component: PaginaNoEncontradaComponent }
+    { path: '**', component: PaginaNoEncontradaComponent },
+
 ];
 
 @NgModule({
